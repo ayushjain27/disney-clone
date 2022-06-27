350c3bf9304c362725faf672e2ef9d3001486315
ayushjain27 <ayushjain201574@gmail.com>
2022-06-27 20:01:05 +0530

    added movies

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,8 +5,47 @@ import Viewers from "./Viewers";
 import NewDisney from "./NewDisney";
 import Originals from "./Originals";
 import Trending from "./Trending";
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import db from "../firebase";
+import { setMovies } from "../features/movies/movieSlice";
+import { selectUserName } from "../features/user/userSlice";
 
 const Home = (props) => {
+  const dispatch = useDispatch();
+  const userName = useSelector(selectUserName);
+  let recommends = [];
+  let newDisneys = [];
+  let originals = [];
+  let trending = [];
+
+  useEffect(() =>{
+    db.collection('movies').onSnapshot((snapshot) => {
+      snapshot.docs.map((doc) => {
+        switch(doc.data().type){
+          case 'recommend':
+              recommends.push({id: doc.id, ...doc.data()})
+              break;
+          case 'new':
+            newDisneys.push({id: doc.id, ...doc.data()})
+              break;
+          case 'original':
+              originals.push({id: doc.id, ...doc.data()})
+              break;
+          case 'trending':
+              trending.push({id: doc.id, ...doc.data()})
+              break;
+        }
+      });
+    });
+    dispatch(setMovies({
+      recommend: recommends,
+      newDisney: newDisneys,
+      original: originals,
+      trending: trending
+    }), [userName]);
+  });
+
   return (
     <Container>
       <ImgSlider />
